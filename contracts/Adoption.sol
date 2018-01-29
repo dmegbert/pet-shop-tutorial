a2c0c4a5005eb64e7020fdab1a99995398b0a112
David Egbert <dmegbert@gmail.com>
2018-01-28 20:44:18 -0500

    Added event emission to contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,10 +2,20 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
     address[16] public adopters;
+
+    // Create an event when an adoption occurs
+    event Adopt(
+        address indexed _adopter,
+        uint _petId
+    );
+
     // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
         adopters[petId] = msg.sender;
+
+        // Emit the event when an adoption occurs
+        Adopt(msg.sender, petId);
         return petId;
     }
     // Retrieving the adopters
